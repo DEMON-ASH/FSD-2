05ddc7dce50d546e2df97969fefd464e17e030f5
DEMON-ASH <theheavenlydemon.20@gmail.com>
2026-01-12 12:55:52 +0530

    updated

---
+++ experiment-2/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+export default function ButtonBasic() {
+  return (
+    <>
+    <Button size="small" variant='outlined'>Small</Button>
+    <Button size="medium" variant='contained'>Medium</Button>
+    <Button size="large" variant='text'>Xl</Button>
+    </>
+  ) 
+
+}
